56f1d3dad0d66eb9d02d3b7b2f654cc60801bf28
Rebelleader <daniel.liddel@icloud.com>
2026-04-15 09:35:33 -0400

    Update RomManagement.tsx
    Added DOS support

--- src/components/RomManagement.tsx
+++ src/components/RomManagement.tsx
@@ -17,7 +17,7 @@ interface Rom {
 const CONSOLES = [
   'NES', 'SNES', 'Game Boy', 'Game Boy Advance', 'Nintendo 64',
   'Nintendo DS', 'Sega Genesis', 'Sega Master System', 'PlayStation',
-  'Atari 2600', 'Arcade',
+  'Atari 2600', 'Arcade', 'DOS',
 ];
 
 export function RomManagement() {
